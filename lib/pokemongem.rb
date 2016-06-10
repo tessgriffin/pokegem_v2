1b1712e88c4b3eb46bd118d2095c4c6b3de26f78
Tess Griffin <tessjgriffin@gmail.com>
2016-06-10 15:07:13 -0400

    Added get_obj method and renamed get_json method
    Added functionality so that you can have an object returned if you don't want a hash returned.
    Changed the name of the other method to match Ruby conventions instead of JavaScript

--- lib/pokemongem.rb
+++ lib/pokemongem.rb
@@ -1,9 +1,16 @@
 require 'net/http'
 require 'json'
+require 'ostruct'
 
 class PokemonGem
-  def self.getJSON(resource, id)
+  def self.get_json(resource, id)
     base_uri = "http://pokeapi.co/api/v2/"
     JSON.parse(Net::HTTP.get(URI(base_uri + "#{resource}/#{id}/")), symbolize_names: true)
+  end
+
+  def self.get_obj(resource, id)
+    base_uri = "http://pokeapi.co/api/v2/"
+    response = JSON.parse(Net::HTTP.get(URI(base_uri + "#{resource}/#{id}/")))
+    OpenStruct.new(response)
   end
 end
--- test/pokemongem_test.rb
+++ test/pokemongem_test.rb
@@ -4,8 +4,21 @@ require_relative '../lib/pokemongem.rb'
 
 class PokemonGemTest < Minitest::Test
   def test_get_json_response
-    type = PokemonGem.getJSON("type", 1)
+    type = PokemonGem.get_json("type", 1)
 
     assert_equal "normal", type[:name]
+  end
+
+  def test_pokemon_json_endpoint_with_name
+    pikachu = PokemonGem.get_json("pokemon", "pikachu")
+
+    assert_equal "pikachu", pikachu[:name]
+    assert_equal 25, pikachu[:id]
+  end
+
+  def test_can_return_object
+    pokemon = PokemonGem.get_obj("pokemon", 25)
+
+    assert_equal "pikachu", pokemon.name 
   end
 end
